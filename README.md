a8aa2353a57947f5f49b8696ff2260aefa2e7418
Gowrimathi S <gowrimathis@syncfusion.com>
2021-08-11 15:34:05 +0530

    Added Range navigator sample for vue 3.

---
+++ README.md
@@ -0,0 +1,6 @@
+The sample demonstrates how to integrate the Syncfusion Range Navigator component into a Vue 3 application using the Vue CLI. The following features are shown in this sample.
+
+1. Series
+2. Slider
+
+
---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+      <ejs-rangenavigator :valueType='valueType' :value='value' :labelFormat='labelFormat'>
+          <e-rangenavigator-series-collection>
+              <e-rangenavigator-series :dataSource='data' type='StepLine' xName='Date' yName='Close' width=2>
+              </e-rangenavigator-series>
+          </e-rangenavigator-series-collection>
+      </ejs-rangenavigator>
+</template>
+
+<script>
+import { RangeNavigatorComponent, StepLineSeries, DateTime, RangenavigatorSeriesCollectionDirective,
+RangenavigatorSeriesDirective } from "@syncfusion/ej2-vue-charts";
+
+export default {
+    name: "App",
+    components: {
+    "ejs-rangenavigator": RangeNavigatorComponent,
+    "e-rangenavigator-series-collection": RangenavigatorSeriesCollectionDirective,
+    "e-rangenavigator-series":  RangenavigatorSeriesDirective
+  },
+  data() {
+    return {
+     valueType: 'DateTime',
+     value: [new Date("2008-01-01"), new Date("2010-01-01")],
+     labelFormat: 'MMM-yy',
+     data:[
+        { Date: new Date("2005-01-01"), Close: 21 },
+        { Date: new Date("2006-01-01"), Close: 24 },
+        { Date: new Date("2007-01-01"), Close: 36 },
+        { Date: new Date("2008-01-01"), Close: 38 },
+        { Date: new Date("2009-01-01"), Close: 54 },
+        { Date: new Date("2010-01-01"), Close: 57 },
+        { Date: new Date("2011-01-01"), Close: 62 },
+      ],
+    };
+  },
+  provide: {
+    rangeNavigator: [ DateTime, StepLineSeries ],
+  },
+};
+</script>
